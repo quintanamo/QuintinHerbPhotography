ed708afb734bc38d6fbb826a136fff5ef3cf3845
Quintanamo <herbquintin@gmail.com>
2023-11-05 22:03:06 -0500

    V2

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,6 @@
-var sections = document.getElementsByClassName('image-section');
-
-for (let item of sections) {
-    item.addEventListener('wheel', function (e) {
-        if (e.deltaY > 0) {
-            item.scrollLeft += 100;
-        } else {
-            item.scrollLeft -= 100;
-        }
-        if (item.scrollLeft > 0 && item.scrollLeft < item.scrollLeftMax) {
-            e.preventDefault();
-        }
-    });
-}
-
 const MODAL = document.getElementById('modal');
 const MODAL_IMAGE = document.getElementById('modal-image');
-let images = document.getElementsByClassName('image-section__image');
+let images = document.getElementsByClassName('gallery__image');
 
 for (let image of images) {
     console.log(image);
